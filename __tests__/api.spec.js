345aca4090bd8bd183aea1cddb9785a1c4190b1e
afeiship <1290657123@qq.com>
2020-07-01 20:54:21 +0800

    fix: use enc/key/value

--- __tests__/api.spec.js
+++ __tests__/api.spec.js
@@ -2,9 +2,15 @@ const nx = require('@feizheng/next-js-core2');
 require('../src/next-spys-port-decode');
 
 describe('api.basic test', () => {
-  test('nx.spysPortDecode', function() {
-    /** 用了 with，暂时无法测试 */
-    var str = '(OneSixEightTwo^Three1Four)+(ZeroNineFourOne^SevenZeroEight)+(OneSixEightTwo^Three1Four)+(ZeroNineFourOne^SevenZeroEight)';
-    console.log(nx.spysPortDecode(str));
+  test('nx.spysPortDecode', function () {
+    var str = '(FourOneNineOne^One5Two)+(ZeroOneTwoNine^Eight8Eight)+(ZeroOneTwoNine^Eight8Eight)+(Three4ZeroSix^NineSixFour)+(EightEightThreeFive^Nine6Five)';
+
+    expect(
+      nx.spysPortDecode({
+        encKey: 'g=D^C;l=B^E;i=F^A;t=6;h=1;f=H^G;j=0;k=2;o=J^y;r=7;m=u^x;c=4;a=z^w;s=v^I;p=9;q=T^S;n=8;b=3;e=5;d=R^M;K=j^l;L=h^i;O=k^g;P=b^a;N=c^d;Q=e^m;W=t^s;X=r^q;V=n^o;U=p^f;',
+        encValue: '^^^^^^^^^^One5Two^Zero^Five^Nine0Three^Three^OneSixOne^ThreeEightZero^Eight^Nine6Five^Two^Four^NineSixFour^Zero9Nine^Seven^Eight8Eight^Six^Five3Six^Nine^One3Seven^One^270^10009^6588^8909^8090^8335^3127^3823^80^4663^808^1513^8118^8724^8088^5609^Three4ZeroSix^EightEightThreeFive^8888^OneOneSevenZero^NineTwoFourThree^FourOneNineOne^Seven4EightTwo^9152^81^525^Zero5SixSeven^ZeroOneTwoNine^FourZeroFiveFour^EightTwoOneEight',
+        encCode: str,
+      })
+    ).toBe("38801");
   });
 });
--- src/next-spys-port-decode.js
+++ src/next-spys-port-decode.js
@@ -1,45 +1,39 @@
 (function () {
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('@feizheng/next-js-core2');
-  var CODE_MAP = {
-    Eight: 8,
-    EightThreeZeroFour: 6661,
-    Five: 6,
-    Five0Three: 6668,
-    Five1Zero: 1975,
-    Five5TwoEight: 7029,
-    Four: 2,
-    FourNineNine: 5714,
-    FourThreeThreeSix: 11320,
-    FourTwoOneFive: 356,
-    Nine: 5,
-    Nine4SixZero: 2021,
-    Nine5SevenSeven: 1971,
-    One: 4,
-    One2FiveNine: 8177,
-    OneSixEightTwo: 11578,
-    Seven: 7,
-    SevenZeroEight: 7603,
-    Six: 0,
-    Three: 1,
-    Three1Four: 11570,
-    ThreeEightNineThree: 5713,
-    ThreeEightOne: 353,
-    Two: 9,
-    TwoNineSeven: 7026,
-    Zero: 3,
-    Zero5Two: 2019,
-    ZeroNineFive: 11322,
-    ZeroNineFourOne: 7603,
-    ZeroSevenSix: 8176,
+  var encFn = function (p, r, o, x, y, s) {
+    y = function (c) {
+      return (c < r ? '' : y(parseInt(c / r))) + ((c = c % r) > 35 ? String.fromCharCode(c + 29) : c.toString(36))
+    }
+    if (!''.replace(/^/, String)) {
+      while (o--) {
+        s[y(o)] = x[o] || y(o)
+      }
+      x = [function (y) {
+        return s[y]
+      }]
+      y = function () {
+        return '\\w+'
+      }
+      o = 1
+    }
+
+    while (o--) {
+      if (x[o]) {
+        p = p.replace(new RegExp('\\b' + y(o) + '\\b', 'g'), x[o])
+      }
+    }
+    return p
   };
 
-  nx.spysPortDecode = function (inString) {
-    if (!inString) return inString;
+  nx.spysPortDecode = function (inOptions) {
     var result = '';
-    with(CODE_MAP){
-      result += eval('":" + ' + inString);
-    }
+
+    eval(
+      encFn(inOptions.encKey, 60, 60, inOptions.encValue.split('\u005e'), 0, {})
+    );
+
+    result += eval('":" + ' + inOptions.encCode);
     return result.slice(1);
   };
 
